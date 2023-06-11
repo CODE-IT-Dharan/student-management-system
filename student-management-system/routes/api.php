89db39717edbd3235b83de94e5210ce328e31415
KirawaYo365 <kirawa.ya.files@gmail.com>
2023-06-11 15:57:23 +0545

    minor changes

--- student-management-system/routes/api.php
+++ student-management-system/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\EnquiryController;
+use App\Http\Controllers\Api\InstallmentController;
 use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -31,6 +32,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('courses', CourseController::class);
     Route::apiResource('enquiries', EnquiryController::class);
     Route::apiResource('admissions', AdmissionController::class);
+    Route::apiResource('installments', InstallmentController::class);
 });
 
 
